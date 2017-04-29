0672d88ceb8f090b51382472055241f37a32b861
Fernando <Fernando@NOTEFERNANDO>
2017-04-29 10:28:22 -0300

    Adicionando classes iniciais do agente.

---
+++ curupira/src/br/ufmg/dcc/cuda/agent/Agent.java
@@ -0,0 +1,12 @@
+package br.ufmg.dcc.cuda.agent;
+
+import java.lang.instrument.Instrumentation;
+
+public class Agent {
+ 
+    public static void premain(String agentArgs, Instrumentation inst) {
+         
+        // registers the transformer
+        inst.addTransformer(new RealClassTransformer());
+    }
+}
